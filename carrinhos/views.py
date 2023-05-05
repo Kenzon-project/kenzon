000485cdbd9284b8ef7b31ffae3a325a40241f86
Julia Pereira <juliapt95@gmail.com>
2023-05-05 10:59:38 -0300

    feat: cria serializer carrinho

--- carrinhos/views.py
+++ carrinhos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .serializers import CarrinhoSerializer
+from .models import Carrinho
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import UserPermissions
 
-# Create your views here.
+
+class CarrinhoView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [UserPermissions]
+
+    queryset = Carrinho.objects.all()
+    serializer_class = CarrinhoSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "user_id"
--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from . import views
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from carrinhos.views import CarrinhoView
 
 urlpatterns = [
     path("users/", views.UserCreate.as_view()),
     path("users/<int:user_id>", views.UserList.as_view()),
     path("users/perfil/<str:username>", views.UserPerfil.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
+    path("users/<int:user_id>/carrinho", CarrinhoView.as_view()),
 ]
